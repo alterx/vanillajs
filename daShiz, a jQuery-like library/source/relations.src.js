c49efb0c49f4f488dfd9737a2073d3f324c97f4d
Carlos Vega <cvega@konradgroup.com>
2014-05-22 14:24:04 -0600

    Step-5

--- daShiz, a jQuery-like library/source/relations.src.js
+++ daShiz, a jQuery-like library/source/relations.src.js
@@ -4,6 +4,32 @@
 
     (function () {
         var plugin = {
+            parent: function () {
+                var result = [];
+                for (var i = 0; i < this.el.length; i++) {
+                    result.push(this.el[i].parentNode);
+                }                
+                this.el = result;                
+                return this;
+            }, 
+            
+            next: function () {
+                var result = [];
+                for (var i = 0; i < this.el.length; i++) {
+                    result.push(this.el[i].nextElementSibling);
+                }
+                this.el = result;
+                return this;
+            },
+            
+            prev: function () {
+                var result = [];
+                for (var i = 0; i < this.el.length; i++) {
+                    result.push(this.el[i].previousElementSibling);
+                }
+                this.el = result;
+                return this;
+            }
         };
         
         da$.fn.extend(da$.fn, plugin);
